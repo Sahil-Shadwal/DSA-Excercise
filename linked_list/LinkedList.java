8414076b69078114a93a63464164cfe09b5bfa64
Sahil-Shadwal <sahilshadwal@gmail.com>
2024-07-01 14:52:17 +0530

    created linked list and created print function

--- linked_list/LinkedList.java
+++ linked_list/LinkedList.java
@@ -27,10 +27,38 @@ public class LinkedList {
         newNode.next = head;
         head = newNode;
     }
+    public void addLast(int data) {
+        Node newNode = new Node(data);
+        if(head == null){
+            head = tail = newNode;
+            return;
+        }
+        tail.next = newNode;
+        tail = newNode;
 
+    }
+
+    public void print() {
+        if(head == null){
+            System.out.println("LL is empty");
+            return;
+        }
+        Node temp = head;
+        while(temp != null) {
+            System.out.print(temp.data + " -> ");
+            temp = temp.next;
+        }
+        System.out.println("null");
+    }
     public static void main(String args[]){
         LinkedList ll = new LinkedList();
         ll.addFirst(2);
+        ll.print();
         ll.addFirst(1);
+        ll.print();
+        ll.addLast(3);
+        ll.print();
+        ll.addLast(4);
+        ll.print();
     }
 }
