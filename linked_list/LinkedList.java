707ae7b0008d817b2fddd7a27d6fc826aa77c89e
Sahil-Shadwal <sahilshadwal@gmail.com>
2024-07-01 14:28:29 +0530

    created addFirst in linked list

--- linked_list/LinkedList.java
+++ linked_list/LinkedList.java
@@ -4,14 +4,33 @@ public class LinkedList {
     public static class Node {
         int data;
         Node next;
-
+        //*constructor 
         public Node(int data){
             this.data = data;
             this.next = null;
         }
     }
+    //* Since there can be only one head and tail 
+    //* so we are defining them as a property in the class */
+    public static Node head;
+    public static Node tail;
+
+    // create new Node
+    // new node's next = head
+    // head = new Node
+    public void addFirst(int data){
+        Node newNode = new Node(data);
+        if(head == null){
+            head = tail = newNode;
+            return;
+        }
+        newNode.next = head;
+        head = newNode;
+    }
 
     public static void main(String args[]){
-        
+        LinkedList ll = new LinkedList();
+        ll.addFirst(2);
+        ll.addFirst(1);
     }
 }
