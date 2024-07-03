97752cd3d827c2a2e020099ed4b5dd4c9b5778cd
Sahil-Shadwal <sahilshadwal@gmail.com>
2024-07-03 09:35:44 +0530

    fucking hell yeah done n queens

--- Backtracking/Queens.java
+++ Backtracking/Queens.java
@@ -1,6 +1,27 @@
 package Backtracking;
 
 public class Queens {
+    public static boolean isSafe(char board[][], int row, int col){
+        //vertical up
+        for(int i = row-1; i>=0; i--){
+            if(board[i][col] == 'Q'){
+                return false;
+            }
+        }
+        //diag left up
+        for(int i=row-1, j =col-1; i>=0 && j>=0; i--,j--){
+            if(board[i][j]=='Q'){
+                return false;
+            }
+        }
+        //diag right up
+        for(int i=row-1, j =col+1; i>=0 && j<board.length; i--,j++){
+            if(board[i][j]=='Q'){
+                return false;
+            }
+        }
+        return true;
+    }
     public static void nQueens(char board[][], int row) {
         //base
         if(row == board.length) {
@@ -9,9 +30,11 @@ public class Queens {
         }
         //column loop
         for(int j = 0; j< board.length; j++){
-            board[row][j] ='Q';
-            nQueens(board, row+1);
-            board[row][j] = 'X';
+            if(isSafe(board, row, j)) {
+                board[row][j] ='Q';
+                nQueens(board, row+1);
+                board[row][j] = 'X';
+            }
         }
     }
     public static void printBoard(char board[][]){
@@ -25,11 +48,11 @@ public class Queens {
     }
 
     public static void main(String args[]){
-        int n = 2;
+        int n = 4;
         char board[][] = new char[n][n];
         for(int i = 0; i < n; i++){
             for(int j = 0; j < n; j++){
-                board[i][j] = '.';
+                board[i][j] = 'X';
             }
         }
         nQueens(board, 0);
