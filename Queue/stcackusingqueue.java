4dc5f15cac15e37b6910aba3f11b7e4e0ac3dc99
Sahil-Shadwal <sahilshadwal@gmail.com>
2024-07-17 12:32:58 +0530

    fix

--- Queue/stcackusingqueue.java
+++ Queue/stcackusingqueue.java
@@ -1,80 +1,84 @@
 package Queue;
 import java.util.*;
 public class stcackusingqueue {
-    static class Stack{
+    static class Stack {
         static Queue<Integer> q1 = new LinkedList<>();
         static Queue<Integer> q2 = new LinkedList<>();
-        
-        public static boolean isEmpty(){
+
+        // empty
+        public static boolean isEmpty() {
             return q1.isEmpty() && q2.isEmpty();
         }
-        public static void push(int data){
-            if(!q1.isEmpty()){
+
+        // add
+        public static void push(int data) {
+            if (!q1.isEmpty()) {
                 q1.add(data);
             } else {
                 q2.add(data);
             }
         }
-        public static int pop(){
-            if(q1.isEmpty()){
+
+        // remove - O(n)
+        public static int pop() { 
+            if (isEmpty()) {
                 System.out.println("Stack is empty");
                 return -1;
             }
+
+            // here there will be two cases as the elements can be in q2 or in q1
             int top = -1;
-            if(!q1.isEmpty()){
-                while(!q1.isEmpty()){
+            if (!q1.isEmpty()) { // case 1
+                while (!q1.isEmpty()) {
                     top = q1.remove();
-                    if(q1.isEmpty()){
+                    if (q1.isEmpty()) {
                         break;
                     }
                     q2.add(top);
-
                 }
-            }else {
-                while(!q2.isEmpty()){
+
+            } else {// case 2
+                while (!q2.isEmpty()) {
                     top = q2.remove();
-                    if(q2.isEmpty()){
+                    if (q1.isEmpty()) {
                         break;
                     }
                     q1.add(top);
-
                 }
+
             }
             return top;
-
         }
-        public static int peek(){
-            if(q1.isEmpty()){
-                System.out.println("Stack is empty");
-                return -1;
-            }
+
+        public static int peek() {
             int top = -1;
-            if(!q1.isEmpty()){
-                while(!q1.isEmpty()){
+            if (!q1.isEmpty()) { // case 1
+                while (!q1.isEmpty()) {
                     top = q1.remove();
-                    
                     q2.add(top);
-
                 }
-            }else {
-                while(!q2.isEmpty()){
+
+            } else {// case 2
+                while (!q2.isEmpty()) {
                     top = q2.remove();
-                    
                     q1.add(top);
-
                 }
+
             }
             return top;
         }
     }
-    public static void main(String args[]){
+
+    public static void main(String args[]) {
         Stack s = new Stack();
         s.push(1);
         s.push(2);
         s.push(3);
-        while(!s.isEmpty()){
+
+        while (!s.isEmpty()) {
             System.out.println(s.peek());
             s.pop();
         }
     }
+
 }
