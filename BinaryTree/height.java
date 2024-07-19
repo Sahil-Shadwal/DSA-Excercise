7cfa1bf3c9e4414b04d855e729dc3a3060f6783a
Sahil-Shadwal <sahilshadwal@gmail.com>
2024-07-19 08:52:12 +0530

    count no of nodes in a binary tree

--- BinaryTree/height.java
+++ BinaryTree/height.java
@@ -20,6 +20,14 @@ public class height {
         int rh = heightBs(root.right);
         return Math.max(lh,rh)+1;
     }
+    public static int countNodes( Node root){
+        if(root == null){
+            return 0;
+        }
+        int lh = countNodes(root.left);
+        int rh = countNodes(root.right);
+        return lh +rh+1;
+    }
     public static void main(String args[]){
     /*
                     1
@@ -36,7 +44,8 @@ public class height {
     root.left.right = new Node(5);
     root.right.left = new Node(6);
     root.right.right = new Node(7);
-
+    root.right.right.right = new Node(19);
     System.out.println(heightBs(root));
+    System.out.println(countNodes(root));
     }
 }
