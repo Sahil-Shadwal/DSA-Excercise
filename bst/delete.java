176b68d1cf879e3cb0005ac964ffa78cf1292106
Sahil-Shadwal <sahilshadwal@gmail.com>
2024-07-23 09:45:48 +0530

    root 2 leaf path print

--- bst/delete.java
+++ bst/delete.java
@@ -1,5 +1,7 @@
 package bst;
 
+import java.util.*;
+
 public class delete {
     static class Node {
         int data;
@@ -83,6 +85,25 @@ public class delete {
             printInRange(root.right, k1, k2);
         }
     }
+    public static void printPath(ArrayList<Integer> path){
+        for(int i = 0; i < path.size(); i++){
+            System.out.print(path.get(i) + "->");
+        }
+        System.out.println("Null");
+    }
+    public static void printRoot2Leaf(Node root, ArrayList<Integer> path){
+        if(root == null){
+            return;
+        }
+        path.add(root.data);
+        if(root.left == null && root.right == null){
+            printPath(path);
+        }
+
+        printRoot2Leaf(root.left, path);
+        printRoot2Leaf(root.right, path);
+        path.remove(path.size()-1);
+    }
     public static void main(String args[]){
         int values[] = {8, 5, 3, 1, 4, 6, 10, 11, 14};
         Node root = null;
@@ -97,6 +118,7 @@ public class delete {
         // System.out.println();
         // inorder(root);
 
-        printInRange(root, 5, 12);
+        // printInRange(root, 5, 12);
+        printRoot2Leaf(root, new ArrayList<>());
     }
 }
