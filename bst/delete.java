a9bf54fb65c66c9306f555c74c5efde5d2214dad
Sahil-Shadwal <sahilshadwal@gmail.com>
2024-07-23 09:21:05 +0530

    print in range

--- bst/delete.java
+++ bst/delete.java
@@ -68,6 +68,21 @@ public class delete {
         System.out.print(root.data+ " ");
         inorder(root.right);
     }
+    public static void printInRange(Node root, int k1, int k2){
+        if(root == null){
+            return;
+        }
+        if(root.data >= k1 && root.data <= k2){
+            printInRange(root.left, k1, k2);
+            System.out.print(root.data + " ");
+            printInRange(root.right, k1, k2);
+        }
+        else if (root.data < k1) {
+            printInRange(root.left, k1, k2);
+        }else {
+            printInRange(root.right, k1, k2);
+        }
+    }
     public static void main(String args[]){
         int values[] = {8, 5, 3, 1, 4, 6, 10, 11, 14};
         Node root = null;
@@ -75,13 +90,13 @@ public class delete {
         for(int i = 0; i< values.length; i++){
             root = insert(root, values[i]);
         }
-        inorder(root);
-        System.out.println();
+        // inorder(root);
+        // System.out.println();
 
-        delete(root ,5);
-        System.out.println();
-        inorder(root);
+        // delete(root ,5);
+        // System.out.println();
+        // inorder(root);
 
-
+        printInRange(root, 5, 12);
     }
 }
