cc882e9167dc82e32041b4f48c60f96fb3982794
Sahil-Shadwal <sahilshadwal@gmail.com>
2024-08-18 09:16:17 +0530

    dfs , bfs and adjacency list

--- Graph/adjacencyList.java
+++ Graph/adjacencyList.java
@@ -16,48 +16,54 @@ public class adjacencyList {
 
         @Override
         public String toString() {
-            return "(" + src + " -> " + dest + ", wt: " + wt + ")";
+            return "(" + src + " -> " + dest + ", weight: " + wt + ")";
+        }
+
+        static void createGraph(ArrayList<Edge>[] graph) {
+            for (int i = 0; i < graph.length; i++) {
+                graph[i] = new ArrayList<>();
+            }
+
+            graph[0].add(new Edge(0, 1, 1));
+            graph[0].add(new Edge(0, 2, 1));
+
+            graph[1].add(new Edge(1, 0, 1));
+            graph[1].add(new Edge(1, 3, 1));
+
+            graph[2].add(new Edge(2, 0, 1));
+            graph[2].add(new Edge(2, 4, 1));
+
+            graph[3].add(new Edge(3, 1, 1));
+            graph[3].add(new Edge(3, 4, 1));
+            graph[3].add(new Edge(3, 5, 1));
+
+            graph[4].add(new Edge(4, 2, 1));
+            graph[4].add(new Edge(4, 3, 1));
+            graph[4].add(new Edge(4, 5, 1));
+
+            graph[5].add(new Edge(5, 6, 1));
+            graph[5].add(new Edge(5, 3, 1));
+            graph[5].add(new Edge(5, 4, 1));
         }
     }
 
-    public static void printNeighbors(ArrayList<Edge>[] graph, int vertex) {
-        System.out.print("Neighbors of vertex " + vertex + ": ");
-        for (Edge edge : graph[vertex]) {
-            System.out.print(edge.dest + " ");
-        }
-        System.out.println();
-    }
-
-    public static void main(String args[]) {
-        int V = 7; // Adjusted the number of vertices
-        ArrayList<Edge>[] graph = new ArrayList[V];
-
-        for (int i = 0; i < V; i++) {
-            graph[i] = new ArrayList<>();
-        }
-
-        // Adding edges
-        graph[0].add(new Edge(0, 1, 5));
-        graph[0].add(new Edge(0, 4, 10));
-        graph[1].add(new Edge(1, 2, 3));
-        graph[1].add(new Edge(1, 3, 2));
-        graph[2].add(new Edge(2, 3, 4));
-        graph[2].add(new Edge(2, 5, 6));
-        graph[3].add(new Edge(3, 4, 1));
-        graph[4].add(new Edge(4, 5, 7));
-        graph[5].add(new Edge(5, 6, 8));
-        graph[6].add(new Edge(6, 0, 9));
-
-        // Printing the graph
-        for (int i = 0; i < V; i++) {
+    public static void printGraph(ArrayList<Edge>[] graph) {
+        for (int i = 0; i < graph.length; i++) {
             System.out.print("Vertex " + i + ": ");
             for (Edge edge : graph[i]) {
                 System.out.print(edge + " ");
             }
             System.out.println();
         }
+    }
 
-        // Example of printing neighbors
-        printNeighbors(graph, 0); // Neighbors of vertex 0
+    public static void main(String args[]) {
+        int V = 6;
+
+        @SuppressWarnings("unchecked")
+        ArrayList<Edge>[] graph = new ArrayList[V];
+
+        Edge.createGraph(graph);
+        printGraph(graph);
     }
 }
