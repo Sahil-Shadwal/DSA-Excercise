52c607c5d4c7f5a5bf332b7a57931a5664d5e262
Sahil-Shadwal <sahilshadwal@gmail.com>
2024-09-02 11:28:18 +0530

    bus fare fix

--- CityBusFare.java
+++ CityBusFare.java
@@ -1,46 +1,44 @@
-import java.util.*;
+import java.util.Scanner;
 
 public class CityBusFare {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
 
-    // Define the path distances in meters
-    static int[] path = { 800, 600, 750, 900, 1400, 1200, 1100, 1500 };
+        String[] arrs = { "TH", "GA", "IC", "HA", "TE", "LU", "NI", "CA" };
+        float[] arr = { 800, 600, 750, 900, 1400, 1200, 1100, 1500 };
 
-    // Define the bus stops in the ring route
-    static String[] busStops = { "TH", "GA", "IC", "HA", "TE", "LU", "NI", "CA" };
+        System.out.println("Available stations:");
+        for (String station : arrs) {
+            System.out.print(station + " ");
+        }
+        System.out.println();
 
-    public static int getFare(String source, String destination) {
-        // Get the index of source and destination in the bus stops array
-        int sourceIndex = Arrays.asList(busStops).indexOf(source);
-        int destinationIndex = Arrays.asList(busStops).indexOf(destination);
+        System.out.print("Enter source station: ");
+        String s = scanner.next().toUpperCase();
+        System.out.print("Enter destination station: ");
+        String d = scanner.next().toUpperCase();
 
-        // Initialize the total distance to 0
-        int totalDistance = 0;
+        float res = 0;
+        int st = -1, ed = -1;
 
-        // Calculate distance based on the circular route
-        if (sourceIndex <= destinationIndex) {
-            for (int i = sourceIndex; i < destinationIndex; i++) {
-                totalDistance += path[i];
-            }
-        } else {
-            // If the destination is before the source, calculate by wrapping around
-            for (int i = sourceIndex; i < path.length; i++) {
-                totalDistance += path[i];
-            }
-            for (int i = 0; i < destinationIndex; i++) {
-                totalDistance += path[i];
-            }
+        for (int i = 0; i < 8; i++) {
+            if (s.equals(arrs[i]))
+                st = i;
+            if (d.equals(arrs[i]))
+                ed = i;
         }
 
-        // Calculate fare based on the distance
-        double fare = (totalDistance / 1000.0) * 5;
+        if (st == ed) {
+            System.out.println("INVALID INPUT");
+        } else {
+            int i = st;
+            while (i != ed) {
+                i = (i + 1) % 8;
+                res += arr[i];
+            }
+            System.out.println((int) Math.ceil(res * 0.005) + " INR");
+        }
 
-        return (int) fare;
-    }
-
-    public static void main(String[] args) {
-        // Example usage
-        System.out.println("Fare from TH to IC: " + getFare("TH", "IC") + " INR");
-        System.out.println("Fare from CA to HA: " + getFare("CA", "HA") + " INR");
-        System.out.println("Fare from NI to HA: " + getFare("NI", "HA") + " INR");
+        scanner.close();
     }
 }
