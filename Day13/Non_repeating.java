365bde1fea07e52152e28f47bee219587e26e539
Sahil-Shadwal <sahilshadwal@gmail.com>
2024-09-24 13:01:29 +0530

    Refactor Non_repeating class for improved efficiency and readability

---
+++ Day13/Non_repeating.java
@@ -0,0 +1,32 @@
+package Day13;
+
+import java.util.*;
+
+public class Non_repeating {
+    public static void non(int arr[], int n) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+
+        for (int i = 0; i < n; i++) {
+            if (map.containsKey(arr[i])) {
+                map.put(arr[i], map.getOrDefault(arr[i], 0) + 1);
+            } else {
+                map.put(arr[i], 1);
+            }
+        }
+        for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
+            if (entry.getValue() == 1) {
+                System.out.println(entry.getKey());
+            }
+        }
+    }
+
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int arr[] = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+        non(arr, n);
+    }
+}
